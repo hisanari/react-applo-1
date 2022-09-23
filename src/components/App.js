6acc62754824ba1f9e3c6af7a78ebb26b8718fd1
hisanari <hisanari.1987@gmail.com>
2022-09-23 12:37:06 +0900

    add paginate

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './../App.css';
 import CreateLink from './CreateLink';
 import Header from './Header';
@@ -12,13 +12,19 @@ function App() {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Routes>
-            <Route path='/' element={<LinkList/>}/>
+            <Route path='/'
+            element={<Navigate replace to="/new/1" />}/>
             <Route
               path="/create"
               element={<CreateLink/>}
             />
             <Route path='/login' element={ <Login /> } />
             <Route path='/search' element={<Search />} />
+            <Route path='/top' element={<LinkList />} />
+            <Route
+              path='/new/:page'
+              element={<LinkList />}
+            />
           </Routes>
         </div>
     </div>
